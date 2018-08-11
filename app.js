eb66160e323f99d21510a5fe8c10de285a70a138
ekeough6 <erickeough6@gmail.com>
2018-08-11 14:12:41 -0400

    added REST api functions

--- app.js
+++ app.js
@@ -3,33 +3,79 @@ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
+//defining constants for the different categories of questions
+const BASIC = 0;
+const BEHAVIORAL = 1;
+const SALARY = 2;
+const BRAINTEASER = 3;
+
+//defining response codes
+const SUCCESS = 0;
+const DATABASE_CONNECTION_ERROR = 100;
+const DATABASE_LOOKUP_ERROR = 101;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/get-count', (req, res) => {
-    const db = new sqlite3.Database("./interviewbot.db");
-    const number = db.get("SELECT count(*) FROM question");
-    db.close();
-    res.render({number});
-});
-
-app.get('/get-question', async (req, res, next) => {
-    const db = new sqlite3.Database("./interviewbot.db");
-    let statement = db.prepare("SELECT question FROM question WHERE type_id = ?");
-    statement.run("SQL STATEMENT", (err, question) => {
-        res.render({});
+    const db = new sqlite3.Database("interviewbot.db", (err) => {
+        if(err) {
+            console.log(err.message);
+            res.send(JSON.stringify({status: DATABASE_CONNECTION_ERROR}));
+        }
+    });
+    db.get("SELECT count(*) FROM question WHERE type_id = ?;", req.category, (err, number) => {
+        if(err) {
+            console.log(err.message);
+            res.send(JSON.stringify({status: DATABASE_LOOKUP_ERROR}));
+        } else {
+            res.send(JSON.stringify({status: SUCCESS, count: number}));
+        }
     });
     db.close();
 });
 
-app.get('/create-question', async(req, res, next) => {
-    const db = new sqlite3.Database("./interviewbot.db");
-    
-    let statement = db.prepare("INSERT INTO question(question,type_id) values(?,?)");
-    statement.run(/*parameters should be question type and question*/);
-    
-    /* Basic = 0, Behavioral = 1, Salary = 2, Brainteaser = 3 */
+app.get('/get-question', (req, res) => {
+    const db = new sqlite3.Database("interviewbot.db", sqlite3.OPEN_READWRITE, (err) => {
+        if(err) {
+            console.log(err.message);
+            res.send(JSON.stringify({status: DATABASE_CONNECTION_ERROR}));
+        }
+    });
 
+    console.log(req);
+    
+    let statement = db.prepare("SELECT question FROM question WHERE type_id = ?;");
+    statement.all(req.body.category, (err, questions) => {
+        console.log(questions);
+        if(err) {
+            console.log(err.message);
+            res.send(JSON.stringify({status: DATABASE_LOOKUP_ERROR}));
+        } else {
+            res.send(JSON.stringify({status: SUCCESS, question: questions[req.body.question]}));
+        }
+    });
+    statement.finalize();
+    db.close();
+});
+
+app.get('/create-question', (req, res) => {
+    const db = new sqlite3.Database("interviewbot.db", (err) => {
+        if(err) {
+            res.send(JSON.stringify({status: DATABASE_CONNECTION_ERROR}));
+        }
+    });
+    
+    let statement = db.prepare("INSERT INTO question(question,type_id) values(?,?);");
+    statement.run(req.body.question, req.body.category, (err) => {
+        if(err) {
+            res.send(JSON.stringify({status: DATABASE_LOOKUP_ERROR}));
+        } else {
+            res.send(JSON.stringify({status: SUCCESS}));
+        }
+    });
+    statement.finalize();
+    
     db.close();
 });
 
