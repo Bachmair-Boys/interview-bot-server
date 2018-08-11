92960362f54aac9276b1617670b7a2984e2e6fe5
ekeough6 <erickeough6@gmail.com>
2018-08-11 11:06:08 -0400

    changed to sqlite

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
+const Promise = require('bluebird');
 const express = require('express');
+const sqlite = require('sqlite');
 
-const Client = require('mariasql');
-
-const DATABASE_HOST = '127.0.0.1';
-const DATABASE_USER = 'root';
-const DATABASE_PASSWORD = 'bachmair';
-const DATABASE_NAME='pickup_game';
-const USERS_TABLE_NAME = 'users';
-
-const SUCCESS = 0;
-const USER_REGISTRATION_ERROR = 101;
-const DATABASE_LOOKUP_ERROR = 102;
-const AUTHENTICATION_ERROR = 103;
-const DATABASE_UPDATE_ERROR = 104;
-const INVALID_TOKEN_ERROR = 105;
 
 const app = express();
+const port = process.env.PORT || 3000;
+const dbPromise = Promise.resolve()
+    .then(() => sqlite.open('./database.sqlite', { Promise }))
+    .then(db => db.migrate({ force: 'last'}));
 
-
-const db = new Client({
-  host: DATABASE_HOST,
-  user: DATABASE_USER,
-  password: DATABASE_PASSWORD,
-  db: DATABASE_NAME
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post();
+app.post("");
 
 module.exports = app;
